8bf56a51c32ffca11da3b164b4cd016240ddc3ba
ttsoncs <tson25102002@gmail.com>
2023-09-04 14:17:44 +0700

    Ready to deploy

--- config/settings.py
+++ config/settings.py
@@ -29,6 +29,9 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') 
 
+ALLOWED_HOSTS = ['localhost', '127.0.0.1'] 
+
+
 # Application definition
 
 INSTALLED_APPS = [
