992c7aa08555f6bcb34504cca19b8dec634d7fc5
ttsoncs <tson25102002@gmail.com>
2023-09-10 00:44:14 +0700

    Update about section

--- static/js/_base.js
+++ static/js/_base.js
@@ -5,10 +5,10 @@ const e2 = document.querySelector('#n2')
 const e3 = document.querySelector('#n3')
 const e4 = document.querySelector('#n4')
 
-const a1 = annotate(e1, { type: 'box', color: '#FF218C', strokeWidth: '1.5' })
-const a2 = annotate(e2, { type: 'box', color: '#FFD800', strokeWidth: '1.5' })
-const a3 = annotate(e3, { type: 'box', color: '#21B1FF', strokeWidth: '1.5' })
-const a4 = annotate(e4, { type: 'underline', color: '#525252', strokeWidth: '1.5', iterations: 1, animate: false, padding: -5 }) 
+const a1 = annotate(e1, { type: 'underline', color: '#FF218C', strokeWidth: '1.5' })
+const a2 = annotate(e2, { type: 'underline', color: '#FFD800', strokeWidth: '1.5' })
+const a3 = annotate(e3, { type: 'underline', color: '#21B1FF', strokeWidth: '1.5' })
+const a4 = annotate(e4, { type: 'underline', color: '#9c9c9c', strokeWidth: '1', iterations: 1, animate: false, padding: -5 }) 
 
 const ag = annotationGroup([a1, a2, a3]);
 ag.show();
