c89004cc87fa6d6d4136250cfdd8810b73cd8a3c
ttsoncs <tson25102002@gmail.com>
2023-09-11 20:37:20 +0700

    Add svg emoji

--- static/js/_base.js
+++ static/js/_base.js
@@ -6,11 +6,11 @@ const e3 = document.querySelector('#n3')
 const e4 = document.querySelector('#n4')
 const e5 = document.querySelector('#n5')
 
-const a1 = annotate(e1, { type: 'underline', color: '#FF218C', strokeWidth: '1.5', padding: -1})
-const a2 = annotate(e2, { type: 'box', color: '#FFD800', strokeWidth: '1.5' })
-const a3 = annotate(e3, { type: 'box', color: '#21B1FF', strokeWidth: '1.5' })
-const a4 = annotate(e4, { type: 'box', color: '#38ef7d', strokeWidth: '1.5' })
-const a5 = annotate(e5, { type: 'underline', color: '#9c9c9c', strokeWidth: '1', iterations: 1, animate: false, padding: -5 }) 
+const a1 = annotate(e1, { type: 'underline', color: '#FF218CCC', strokeWidth: '1.5' })
+const a2 = annotate(e2, { type: 'underline', color: '#FFD800CC', strokeWidth: '1.5' })
+const a3 = annotate(e3, { type: 'underline', color: '#21B1FFCC', strokeWidth: '1.5' })
+const a4 = annotate(e4, { type: 'underline', color: '#38ef7dCC', strokeWidth: '1.5' })
+const a5 = annotate(e5, { type: 'underline', color: '#9c9c9c', strokeWidth: '1', iterations: 1, animate: false, padding: -5 })
 
 const ag = annotationGroup([a1, a2, a3, a4]);
 ag.show();
