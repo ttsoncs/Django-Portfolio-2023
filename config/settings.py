1789af8b9adfe71b7302385de784ceb771f5e48f
ttsoncs <tson25102002@gmail.com>
2023-09-12 17:55:21 +0700

    Move back to .venv

--- config/settings.py
+++ config/settings.py
@@ -40,12 +40,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
     "tailwind",
     "theme",
     "django_browser_reload",
     "whitenoise.runserver_nostatic",
     "pages",
 ]
+
+SITE_ID = 1
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
