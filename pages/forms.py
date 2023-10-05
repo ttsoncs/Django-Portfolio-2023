1c1bdd0ac9c23c5c128cae30cdb2897f6d5fb7d6
ttsoncs <tson25102002@gmail.com>
2023-10-05 16:34:00 +0700

    Add reply email

--- pages/forms.py
+++ pages/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    from_email = forms.EmailField(required=True)
-    subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea, required=True)
+    user_name = forms.CharField(required=True)
+    user_email = forms.EmailField(required=True)
+    user_subject = forms.CharField(required=True)
+    user_message = forms.CharField(widget=forms.Textarea, required=True)
--- pages/views.py
+++ pages/views.py
@@ -1,10 +1,10 @@
 from django.views.generic import View, TemplateView, ListView, DetailView
 
+from django.conf import settings
 from django.core.mail import send_mail, BadHeaderError
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import ContactForm
-
 
 from .models import Project, Post
 
@@ -44,11 +44,16 @@ class ContactPageView(View):
     def post(self, request):
         form = ContactForm(request.POST)
         if form.is_valid():
-            subject = form.cleaned_data["subject"]
-            from_email = form.cleaned_data["from_email"]
-            message = form.cleaned_data["message"]
+            user_name = form.cleaned_data["user_name"]
+            user_email = form.cleaned_data["user_email"]
+            user_subject = form.cleaned_data["user_subject"]
+            user_message = f'From: {user_email} \nMessage: {form.cleaned_data["user_message"]}'
+            my_email = settings.DEFAULT_FROM_EMAIL
+            my_subject = "Thank You for Reaching Out"
+            my_message = f"Dear {user_name},\n\nThank you for contacting me through my portfolio. I wanted to acknowledge that I have received your message and will be responding to you shortly.\n\nI appreciate your interest and patience as I review your inquiry. I understand the importance of your message and will provide a prompt and thorough response.\n\nThank you for considering my portfolio, and I look forward to assisting you further.\n\nBest regards,\n\nSơn"
             try:
-                send_mail(subject, message, from_email, ["ttson.cs@gmail.com"])
+                send_mail(user_subject, user_message, my_email, [my_email])
+                send_mail(my_subject, my_message, my_email, [user_email])
             except BadHeaderError:
                 return HttpResponse("Invalid header found.")
             return redirect("success")
